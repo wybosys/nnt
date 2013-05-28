71f2940925e5869dcd5e74963c003e3f0b1d8d9c
wybosys <wybosys@gmail.com>
2013-05-28 12:13:15 +0800

    cuda

---
+++ bin/cuda-find.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os, re
+
+def find_macos(base):
+    if (os.path.exists(base) == False):
+        return None
+    pat = re.compile("CUDA-(.+)")
+    allver = []
+    for dir in os.listdir(base):
+        full = base + dir
+        if (os.path.isdir(full) == False):
+            continue
+        vers = pat.findall(dir)
+        if (len(vers) == 0):
+            continue
+        ver = vers[0]
+        allver.append(ver)
+    allver = sorted(allver)
+    return base + "CUDA-" + allver[0]
+
+def main():
+    dir = find_macos("/Developer/NVIDIA/")
+    if (dir != None):
+        return dir
+    return ""
+
+print(main())
