37e0e373e015b71285b2d1d54a7321fe3a0b4831
wybosys <wybosys@gmail.com>
2013-07-27 15:58:38 +0800

    audio

---
+++ nnt/Codec/AudioStream.h
@@ -0,0 +1,71 @@
+
+# ifndef __NNT_AUDIOSTREAM_994260C3757F40CB958C6BAAE2670905_H_INCLUDED
+# define __NNT_AUDIOSTREAM_994260C3757F40CB958C6BAAE2670905_H_INCLUDED
+
+# ifdef NNT_TARGET_IOS
+
+class AudioStreamBasicDescription;
+
+# endif
+
+NNT_BEGIN_HEADER_CXX
+
+class AbstractPCMAudioStream
+: public cxx::Object<>
+{
+public:
+    
+    AbstractPCMAudioStream();
+    ~AbstractPCMAudioStream();
+    
+    // 读取一个文件
+    virtual bool load(core::string const&) = 0;
+    
+};
+
+# ifdef NNT_TARGET_IOS
+
+class PCMAudioStreamIOS
+: public AbstractPCMAudioStream
+{
+public:
+    
+    PCMAudioStreamIOS();
+    ~PCMAudioStreamIOS();
+    
+    virtual bool load(core::string const&);
+    
+protected:
+    
+    AudioStreamBasicDescription* _ofmt;
+    core::data _buf;
+    
+    friend class OpenALAudioPlayer;
+};
+
+typedef PCMAudioStreamIOS PCMAudioStream;
+
+# endif
+
+# ifdef NNT_TARGET_ANDROID
+
+class PCMAudioStreamAndroid
+: public AbstractPCMAudioStream
+{
+public:
+    
+    PCMAudioStreamAndroid();
+    ~PCMAudioStreamAndroid();
+    
+    virtual bool load(CCString const&);
+    
+};
+
+typedef PCMAudioStreamAndroid PCMAudioStream;
+
+# endif
+
+NNT_END_HEADER_CXX
+
+# endif
+
