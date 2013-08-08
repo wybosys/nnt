5ff7c9f5b3a96f855ee34cbbf6eda202cdcc2100
huangbaoyu <baoyuhuang@163.com>
2013-08-08 20:04:33 +0800

    openapi

--- nnt/OpenApi/OARenren.h
+++ nnt/OpenApi/OARenren.h
@@ -42,6 +42,35 @@ NNT_BEGIN_HEADER_OBJC
 
 @end
 
+
+// renren API V2.0
+@interface OApiRenRen_2_0 : OACommonApi_2_0
+
+@end
+
+@interface OApiRenRenUserInfo_2_0 : OApiRenRen_2_0
+
+@end
+
+@interface OApiRenRenUserLoginGet_2_0 : OApiRenRen_2_0
+
+@property (nonatomic, readonly) NSString *user_id, *nickname;
+
+@end
+
+typedef enum {
+    PRIVATE,    //仅自己可见
+    PUBLIC,     //所有人可见
+    PASSWORD,   //密码访问可见
+    FRIEND,     //仅好友可见
+} AccessControlType;
+
+@interface OApiRenRenBlogPut_2_0 : OApiRenRen_2_0
+
+@property (nonatomic, copy) NEED NSString *title, *content;
+
+@end
+
 NNT_END_HEADER_OBJC
 
 # ifdef NNT_CXX
@@ -54,6 +83,9 @@ typedef oauth::Provider<OARenRen> Provider;
 NNT_BEGIN_NS(sns)
 
 typedef oauth::Function<OApiRenRenUserInfo> Userinfo;
+typedef oauth::Function<OApiRenRenUserInfo_2_0> Userinfo_2_0;
+typedef oauth::Function<OApiRenRenUserLoginGet_2_0> UserLoginGet_2_0;
+typedef oauth::Function<OApiRenRenBlogPut_2_0> BolgPut_2_0;
 
 class Post
 : public oauth::PostFunction<OApiRenRenWeiboPost>
@@ -70,6 +102,22 @@ public:
         this->_self.content = str;
     }
     
+};
+
+class BlogPut
+: public oauth::PostFunction<OApiRenRenBlogPut_2_0>
+{
+public:
+    
+    void set_title(ns::String const& str)
+    {
+        this->_self.title = str;
+    }
+    
+    void set_content(ns::String const& str)
+    {
+        this->_self.content = str;
+    }
 };
 
 NNT_END_NS
