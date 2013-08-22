a42a27e82358a4bf086816428bb9c1b8dc22095d
wybosys <wybosys@gmail.com>
2013-08-22 12:50:03 +0800

    sqlite

--- contrib/sqlite/sqlite3+nnt.c
+++ contrib/sqlite/sqlite3+nnt.c
@@ -21,67 +21,97 @@ extern void* sqlite3PagerXCodec(void*, void*, Pgno, int);
 extern void sqlite3PagerXCodecSizeChanged(void*, int, int);
 extern void sqlite3PagerXCodecFree(void*);
 
-# if defined(NNT_MACH)
-
-#   define sqlite_aes_t nsaes_t
-#   define sqlite_aes_free nsaes_free
-#   define sqlite_aes_new nsaes_new
-#   define sqlite_aes_set_padding nsaes_set_padding
-#   define sqlite_aes_set_key nsaes_set_key
-#   define sqlite_aes_encrypt nsaes_encrypt
-#   define sqlite_aes_decrypt nsaes_decrypt
-#   define sqlite_aes_swap_rw nsaes_swap_rw
-#   define sqlite_aes_set_enkey nsaes_set_enkey
-#   define sqlite_aes_set_dekey nsaes_set_dekey
-
-# else
-
-#   define sqlite_aes_t aes_t
-#   define sqlite_aes_free aes_free
-#   define sqlite_aes_new aes_new
-#   define sqlite_aes_set_padding aes_set_padding
-#   define sqlite_aes_set_key aes_set_key
-#   define sqlite_aes_encrypt aes_encrypt
-#   define sqlite_aes_decrypt aes_decrypt
-#   define sqlite_aes_swap_rw aes_swap_rw
-#   define sqlite_aes_set_enkey aes_set_enkey
-#   define sqlite_aes_set_dekey aes_set_dekey
-
-# endif
-
 typedef struct
 {
-    sqlite_aes_t* key;
+    void* key;
     size_t pager_size;
     Pager* pager;
-} sqlite3_crypto_t;
+}
+sqlite3_crypto_t;
+
+typedef void* (*func_new_t)();
+static func_new_t func_new;
+
+typedef void (*func_free_t)(void*);
+static func_free_t func_free;
+
+typedef void (*func_set_padding_t)(void*, int);
+static func_set_padding_t func_set_padding;
+
+typedef void (*func_swap_rw_t)(void*);
+static func_swap_rw_t func_swap_rw;
+
+typedef int (*func_set_key_t)(void*, void const*, size_t);
+static func_set_key_t func_set_key;
+
+typedef int (*func_encrypt_t)(void*, void const*, size_t, void**, size_t*);
+static func_encrypt_t func_encrypt;
+
+typedef int (*func_decrypt_t)(void*, void const*, size_t, void**, size_t*);
+static func_decrypt_t func_decrypt;
+
+typedef void (*func_set_enkey_t)(void*, void const*);
+static func_set_enkey_t func_set_enkey;
+
+typedef void (*func_set_dekey_t)(void*, void const*);
+static func_set_dekey_t func_set_dekey;
+
+static void sqlite3_crypto_openssl()
+{
+    func_new = (func_new_t)&aes_new;
+    func_free = (func_free_t)&aes_free;
+    func_set_padding = (func_set_padding_t)&aes_set_padding;
+    func_swap_rw = (func_swap_rw_t)&aes_swap_rw;
+    func_set_key = (func_set_key_t)&aes_set_key;
+    func_encrypt = (func_encrypt_t)&aes_encrypt;
+    func_decrypt = (func_decrypt_t)&aes_decrypt;
+    func_set_enkey = (func_set_enkey_t)&aes_set_enkey;
+    func_set_dekey = (func_set_dekey_t)&aes_set_dekey;
+}
+
+static void sqlite3_crypto_ns()
+{
+# ifdef NNT_MACH
+    
+    func_new = (func_new_t)&nsaes_new;
+    func_free = (func_free_t)&nsaes_free;
+    func_set_padding = (func_set_padding_t)&nsaes_set_padding;
+    func_swap_rw = (func_swap_rw_t)&nsaes_swap_rw;
+    func_set_key = (func_set_key_t)&nsaes_set_key;
+    func_encrypt = (func_encrypt_t)&nsaes_encrypt;
+    func_decrypt = (func_decrypt_t)&nsaes_decrypt;
+    func_set_enkey = (func_set_enkey_t)&nsaes_set_enkey;
+    func_set_dekey = (func_set_dekey_t)&nsaes_set_dekey;
+    
+# endif
+}
 
 static void sqlite3_free_crypto(sqlite3_crypto_t* cpt)
 {
-    sqlite_aes_free(cpt->key);
+    func_free(cpt->key);
     free(cpt);
 }
 
 static sqlite3_crypto_t* sqlite3_new_crypto(void const* skey, int lkey)
 {
     sqlite3_crypto_t* cpt = (sqlite3_crypto_t*)calloc(1, sizeof(sqlite3_crypto_t));
-    cpt->key = sqlite_aes_new();
-    if (sqlite_aes_set_key(cpt->key, skey, lkey) != 0) {
+    cpt->key = func_new();
+    if (func_set_key(cpt->key, skey, lkey) != 0) {
         sqlite3_free_crypto(cpt);
         return NULL;
     }
-    sqlite_aes_set_padding(cpt->key, false);
+    func_set_padding(cpt->key, false);
     return cpt;
 }
 
 static int sqlite3_crypto_encrypt(sqlite3_crypto_t* cpt, void* indata, size_t inlen, void** outdata, size_t* outlen)
 {
-    return sqlite_aes_encrypt(cpt->key, indata, inlen, outdata, outlen);
+    return func_encrypt(cpt->key, indata, inlen, outdata, outlen);
 }
 
 static int sqlite3_crypto_decrypt(sqlite3_crypto_t* cpt,void* indata, size_t inlen, void** outdata, size_t* outlen)
 {
-    return sqlite_aes_decrypt(cpt->key, indata, inlen, outdata, outlen);
+    return func_decrypt(cpt->key, indata, inlen, outdata, outlen);
 }
 
 static int sqlite3_key_interop(sqlite3* db, void const* skey, int lkey)
@@ -97,7 +127,7 @@ static int sqlite3_rekey_interop(sqlite3* db, void const* skey, int lkey)
     sqlite3_crypto_t* oldcrypt = sqlite3PagerGetCodec(p);
     sqlite3_crypto_t* newcrypt = sqlite3_new_crypto(skey, lkey);
     
-    sqlite_aes_set_dekey(newcrypt->key, TRIEXP(oldcrypt, oldcrypt->key, NULL));
+    func_set_dekey(newcrypt->key, TRIEXP(oldcrypt, oldcrypt->key, NULL));
     
 	int rc = sqlite3BtreeBeginTrans(pbt, 1);
     
@@ -171,9 +201,9 @@ void* sqlite3PagerXCodec(void* codec, void* data, Pgno pno, int nmode)
         } break;
         case 7: // encrypt a page for the journal file
         {
-            sqlite_aes_swap_rw(cpt->key);
+            func_swap_rw(cpt->key);
             sqlite3_crypto_encrypt(cpt, data, cpt->pager_size, &buf, &lbuf);
-            sqlite_aes_swap_rw(cpt->key);
+            func_swap_rw(cpt->key);
         } break;
     }
     
@@ -231,10 +261,28 @@ void sqlite3_activate_see()
 
 int sqlite3_key(sqlite3* db, void const* skey, int lkey)
 {
+    sqlite3_crypto_openssl();
     return sqlite3_key_interop(db, skey, lkey);
 }
 
 int sqlite3_rekey(sqlite3* db, void const* skey, int lkey)
 {
+    sqlite3_crypto_openssl();
     return sqlite3_rekey_interop(db, skey, lkey);
 }
+
+# ifdef NNT_MACH
+
+int sqlite3_ns_key(sqlite3* db, void const* skey, int lkey)
+{
+    sqlite3_crypto_ns();
+    return sqlite3_key_interop(db, skey, lkey);
+}
+
+int sqlite3_ns_rekey(sqlite3* db, void const* skey, int lkey)
+{
+    sqlite3_crypto_ns();
+    return sqlite3_rekey_interop(db, skey, lkey);
+}
+
+# endif
